4515d2a38b3c86a8fbfa4e6000ee4474bf6ccb46
SaraYasser28 <161871991+SaraYasser28@users.noreply.github.com>
2025-09-01 15:31:51 +0300

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:booky_library/main.dart';
+import 'package:booky/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
